3564df3b3c462395b559ecbcb52f38201df3219e
kobietdatten100-sketch <kobietdatten100@gmail.com>
2026-06-25 14:46:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const client = new Client({intents: [GatewayIntentBits.Guilds,GatewayIntentBits.
 
 const PREFIX = "!";
 
-client.once("ready", () => {
-    console.log(`✅ ${client.user.tag} đã online`);
-});
+client.once("ready", () => {console.log(✅ ${client.user.tag} đã online);});
 
 client.on("messageCreate", async (message) => {
 
@@ -26,11 +24,11 @@ if (
 const content = message.content.slice(3).trim();
 
 if (!content) {
-    return message.reply(" Vui lòng nhập nội dung thông báo.");
+    return message.reply("❌ Vui lòng nhập nội dung thông báo.");
 }
 
 const embed = new EmbedBuilder()
-    .setTitle(" THÔNG BÁO")
+    .setTitle("📢 THÔNG BÁO")
     .setDescription(content)
     .setColor("#0099ff")
     .setFooter({
@@ -47,7 +45,3 @@ await message.delete().catch(() => {});
 });
 
 client.login(process.env.TOKEN);
-
-Tên file: .gitignore
-
-node_modules.env
